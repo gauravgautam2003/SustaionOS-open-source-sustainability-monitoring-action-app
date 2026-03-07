05415ffde987a3aeccad6a1ad118c56a60c908e0
Gautam sagar <gateaspirant8650@gmail.com>
2026-03-07 23:20:36 +0530

    Add prediction engine in server

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use("/api/data",require("./routes/data.routes"));
 app.use("/api/alerts",require("./routes/alert.routes"));
 app.use("/api/analytics",require("./routes/analytics.routes"));
+app.use("/api/predict",require("./routes/prediction.routes"));
 
 app.use(require("./middleware/error.middleware"));
 
--- server/services/prediction.service.js
+++ server/services/prediction.service.js
@@ -1,4 +1,11 @@
-exports.predict=(values)=>{
- const avg=values.reduce((a,b)=>a+b,0)/values.length;
- return Math.round(avg);
-}
+exports.predictNext = (records) => {
+ if(!records.length) return null;
+
+ const totalWater = records.reduce((sum,r)=>sum+r.water,0);
+ const totalEnergy = records.reduce((sum,r)=>sum+r.energy,0);
+
+ return {
+  predictedWater: Math.round(totalWater/records.length),
+  predictedEnergy: Math.round(totalEnergy/records.length)
+ };
+};
