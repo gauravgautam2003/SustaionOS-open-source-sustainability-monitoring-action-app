6b58879ce010d96953810ba70ae6f2ca27eef22e
gauravgautam2003 <gauravgautam9865@gmail.com>
2026-03-07 23:31:50 +0530

    client full structure created

---
+++ client/src/components/dashboard/SuggestionPanel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuggestionPanel = () => {
+  return (
+    <div>SuggestionPanel</div>
+  )
+}
+
+export default SuggestionPanel
--- client/src/pages/Landing.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SustainabilityReport = () => (
-  <div>Sustainability Report</div>
-);
-
-export default SustainabilityReport;
